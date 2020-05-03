dc6b48333804fd42ada0a0232d7bf42b36be446c
Snickdx <snickdx@gmail.com>
2020-05-03 17:13:03 -0400

    frist commit

---
+++ main.py
@@ -0,0 +1,125 @@
+import json
+from flask import Flask, request
+from flask_cors import CORS
+from flask_jwt import JWT, jwt_required, current_identity
+from sqlalchemy.exc import IntegrityError
+from datetime import timedelta 
+
+from models import db, User, Todo
+
+''' Begin boilerplate code '''
+def create_app():
+  app = Flask(__name__)
+  app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+  app.config['SECRET_KEY'] = "MYSECRET"
+  app.config['JWT_EXPIRATION_DELTA'] = timedelta(days = 7) 
+  CORS(app)
+  db.init_app(app)
+  return app
+
+app = create_app()
+
+app.app_context().push()
+db.create_all(app=app)
+''' End Boilerplate Code '''
+
+''' Set up JWT here '''
+def authenticate(uname, password):
+  #search for the specified user
+  user = User.query.filter_by(username=uname).first()
+  #if user is found and password matches
+  if user and user.check_password(password):
+    return user
+
+#Payload is a dictionary which is passed to the function by Flask JWT
+def identity(payload):
+  print(payload)
+  return User.query.get(payload['identity'])
+
+jwt = JWT(app, authenticate, identity)
+
+''' End JWT Setup '''
+
+
+@app.route('/')
+def index():
+  return app.send_static_file('index.html')
+
+@app.route('/app')
+def client_app():
+  return app.send_static_file('app.html')
+
+
+@app.route('/signup', methods=['GET'])
+def signup_page():
+  return app.send_static_file('signup.html')
+
+
+@app.route('/signup', methods=['POST'])
+def signup():
+  userdata = request.get_json() # get userdata
+  newuser = User(username=userdata['username'], email=userdata['email']) # create user object
+  newuser.set_password(userdata['password']) # set password
+  try:
+    db.session.add(newuser)
+    db.session.commit() # save user
+  except IntegrityError: # attempted to insert a duplicate user
+    db.session.rollback()
+    return 'username or email already exists' # error message
+  return 'user created' # success
+
+@app.route('/identify')
+@jwt_required()
+def protected():
+    return json.dumps(current_identity.username)
+
+@app.route('/todo', methods=['POST'])
+@jwt_required()
+def create_todo():
+  data = request.get_json()
+  todo = Todo(text=data['text'], userid=current_identity.id, done=False)
+  db.session.add(todo)
+  db.session.commit()
+  return json.dumps(todo.id), 201 # return data and set the status code
+
+@app.route('/todo', methods=['GET'])
+@jwt_required()
+def get_todos():
+  todos = Todo.query.filter_by(userid=current_identity.id).all()
+  todos = [todo.toDict() for todo in todos] # list comprehension which converts todo objects to dictionaries
+  return json.dumps(todos)
+
+@app.route('/todo/<id>', methods=['GET'])
+@jwt_required()
+def get_todo(id):
+  todo = Todo.query.filter_by(userid=current_identity.id, id=id).first()
+  if todo == None:
+    return 'Invalid id or unauthorized'
+  return json.dumps(todo.toDict())
+
+@app.route('/todo/<id>', methods=['PUT'])
+@jwt_required()
+def update_todo(id):
+  todo = Todo.query.filter_by(userid=current_identity.id, id=id).first()
+  if todo == None:
+    return 'Invalid id or unauthorized'
+  data = request.get_json()
+  if 'text' in data: # we can't assume what the user is updating wo we check for the field
+    todo.text = data['text']
+  if 'done' in data:
+    todo.done = data['done']
+  db.session.add(todo)
+  db.session.commit()
+  return 'Updated', 201
+
+@app.route('/todo/<id>', methods=['DELETE'])
+@jwt_required()
+def delete_todo(id):
+  todo = Todo.query.filter_by(userid=current_identity.id, id=id).first()
+  if todo == None:
+    return 'Invalid id or unauthorized'
+  db.session.delete(todo) # delete the object
+  db.session.commit()
+  return 'Deleted', 204
+
+app.run(host='0.0.0.0', port=8080)
---
+++ readme.md
@@ -0,0 +1,73 @@
+[![Gitpod Ready-to-Code](https://img.shields.io/badge/Gitpod-Ready--to--Code-blue?logo=gitpod)](https://gitpod.io/#https://github.com/uwidcit/info2602ps.git) 
+
+# Before you start
+Create a heroku account by following this [link](https://signup.heroku.com/login)
+You will also need a github account.
+
+# Creating & Linking your own github repository
+Git is used to perform source control. It helps teams manage code bases and merge togher their contributions.
+Github is a free Git server for developers to host their git repositories.
+
+When the project is launched on gitpod, it will already be linked to a starter github repository. To link it to a new repository you need to first run the following command.
+
+```
+rm - rf .git
+```
+Now you can initialize a new git workspace by executing the following.
+
+
+```
+git init
+```
+
+Next, create your respository on github by following this [link](https://github.com/new). Give it a name and click on the green "Create Repository" button below.
+The next page would give you instructions on how to link the repository to an existing workspace.
+
+Copy the command which looks like.
+
+```
+git remote add origin https://github.com/<username>/<reponame>.git
+```
+Paste and run the command in gitpod.
+
+Your workspace should now be linked to your repository
+
+# Heroku Setup
+To login to heroku open a new terminal in gitpod and enter run the following command:
+
+```
+heroku login --interactive
+```
+You will then be prompted to enter you heroku credentials. Then to create a new heroku application run the following command:
+
+```
+heroku create
+```
+The app should be created with a random name and can be seen on your [heroku dashboard](https://dashboard.heroku.com/apps/)
+
+# Pushing changes to Git
+Now the workspace is linked with a git repository it will track all the changes you make to its files.
+Run the following command to select which files will be **staged** (tracked for changes)
+
+```
+git stage *
+```
+
+This will stage all files in the workspace.
+Then You prepare the changes to be sent to the repository by making a **commit**. You must supply a custom commit message when making commits.
+Run the following command
+
+```
+git commit -m "first commit"
+```
+
+Finally you can send your changes to the repository by performing a **push**.
+
+```
+git push -u origin master
+```
+
+Heroku is setup to auto deploy the application for every push to the master branch the github repo. You can view the application by opening it from your [heroku dashboard](https://dashboard.heroku.com/apps/). Click on the newly created app then click on the "Open App" button.
+
+You application should now be deployed on heroku!
+
