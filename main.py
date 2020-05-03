59e4f22f2b55aeb914ad1b83377431aca5326d59
Nicholas Mendez <snickdx@gmail.com>
2020-05-03 23:05:10 +0000

    fixed run command

--- main.py
+++ main.py
@@ -115,4 +115,5 @@ def delete_todo(id):
   db.session.commit()
   return 'Deleted', 204
 
-app.run(host='0.0.0.0', port=8080)
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=8080, debug=True)
